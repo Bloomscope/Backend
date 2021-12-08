4343297afdc830bcb2be1629515d66dd13dd5473
alderson41 <aldersonanirudh@gmail.com>
2021-12-08 15:29:00 +0530

    added parent stuffs

--- app/parents/routes.py
+++ app/parents/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from ..model import *
 from flask_jwt_extended import jwt_required
 from ..utils.decorators import current_user_proxy_obj as current_user
+from datetime import datetime
 
 
 parent = Blueprint('parent', __name__)
@@ -31,4 +32,23 @@ def child_info():
 @jwt_required()
 def get_child_suggestion():
     cid = Parent_Child.query.filter_by(parent_id=current_user().id).first()
-    return jsonify({'data': [i.as_dict() for i in Suggestions.query.filter_by(student_id=cid).all()]})
+    return jsonify({'data': [i.as_dict() for i in Suggestions.query.filter_by(student_id=cid.user_id).all()]})
+
+
+@parent.route('/api/get_child_tests')
+@jwt_required()
+def get_child_tests():
+    cid = Parent_Child.query.filter_by(parent_id=current_user().id).first().user_id
+    tests = TestSchedule.query.filter_by(user_id=cid).all()
+    resp = {'data': []}
+    for test in tests:
+        data = test.as_dict()
+        if datetime.utcnow() > test.starts_on or datetime.utcnow() > test.ends_on:
+            data['date'] = 0
+        if datetime.utcnow() < test.starts_on:
+            data['date'] = -1
+        if datetime.utcnow() > test.ends_on:
+            data['date'] = 1
+        data['name'] = Test.query.filter_by(id=test.test_id).first().name
+        resp['data'].append(data)
+    return jsonify(resp)
