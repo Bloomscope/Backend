a764b2d249f10bbdf98116d03220d5afc54c101d
alderson41 <aldersonanirudh@gmail.com>
2021-12-11 09:07:48 +0530

    added parametric wise results

--- app/utils/evaluator.py
+++ app/utils/evaluator.py
@@ -46,6 +46,8 @@ class Eval:
         add parameter wise result calculation
         '''
         self.__pre_process()
+        all_params = set(i['param_name'] for i in self.data['quiz_data'])
+        params = {i: {"param_name": i, "total_questions": 0, "correct_resp": 0} for i in all_params}
         with app().app_context():
             for question in self.data['quiz_data']:
                 quest = model.Questions.query.filter_by(id=question['question_id']).first()
@@ -62,6 +64,8 @@ class Eval:
                         'param_id': question['param_id'],
                         'param_name': question['param_name']
                     })
+                    params[question['param_name']]['correct_resp'] += 1
+                    params[question['param_name']]['total_questions'] += 1
                 else:
                     self.total_marks += quest.marks
                     self.resp['result'].append({
@@ -73,10 +77,12 @@ class Eval:
                         'param_id': question['param_id'],
                         'param_name': question['param_name']
                     })
+                    params[question['param_name']]['total_questions'] += 1
         self.resp['test_id'] = self.test_id
         self.resp['total_marks'] = self.total_marks
         self.resp['result_quest'] = f'{self.correctly_answered}/{self.total_questions}'
         self.resp['result_marks'] = f'{self.marks_scored}/{self.total_marks}'
+        self.resp['param_wise_result'] = params
         self.__write_results()
     
         return self.resp
