9927028265fe37cb8adafbf35bd25f994f152c08
Blazeleo <satviksams@gmail.com>
2022-02-08 13:24:03 +0530

    v1.0

--- app/config.py
+++ app/config.py
@@ -4,14 +4,16 @@ from datetime import timedelta
 SECRET_KEY = 'bloomscope@iitb'
 
 # sqlite db on dev env
-db_path = os.path.join(os.path.dirname(__file__), 'database.db')
-db_uri = 'sqlite:///{}'.format(db_path)
+# db_path = os.path.join(os.path.dirname(__file__), 'database.db')
+# db_uri = 'sqlite:///{}'.format(db_path)
 
 # mysql db for prod or test
 # db_uri = 'mysql://root:anirudhmp@127.0.0.1:3306/proto'
 
 # postgress db for prod
 # db_uri = 'postgresql://kxhdamszearupc:fc2a0b818f146c2e12d0428327f5d54f254a6f6591e39d14a750d51f589c814b@ec2-52-0-93-3.compute-1.amazonaws.com:5432/d5lksdo86sshue'
+
+db_uri = 'postgres://zltlwenpvgofmb:d743094f5619bf920edef02f951d07cea9ac75ac0c9e8289158dbc8ee9fd522b@ec2-18-215-8-186.compute-1.amazonaws.com:5432/d235ps2p864i36'
 
 WTF_CSRF_ENABLED = False
 SQLALCHEMY_DATABASE_URI = db_uri 
